ba52654b4b0c6509c1736c24fa3ca9a575cbe0b6
Thanh-ziki <thanh.luongvan2k@gmail.com>
2022-09-21 15:49:31 +0700

    21/9HTTH

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using DemoMvc.Models;
+namespace DemoMvc.Controllers;
+public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Person> stdList= new List<Person>()
+        {
+            new Person {ID = "số", Password= 3, NickName = 36},
+            new Person {ID = "dá", Password= 2, NickName = 75}
+            new Person {ID = "dá", Password= 2, NickName = 75}
+        };
+        ViewData["Người Dùng"] = stdList;
+        return View();
+     }
+     [HttpPost]
+     public IActionResult Create(ID std)
+
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,1 +1,8 @@
-namespace
+namespace DemoMvc.Models;
+public class Person
+{
+    public string ID {get; set;}
+    public int Password {get;set;}
+    public int NickName {get;set;}
+
+}
